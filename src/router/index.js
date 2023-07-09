309d57f6a292e02018892bc6d258f6ecd554b3cb
Tomáš Reinert <reinerttomas@gmail.com>
2023-07-09 10:13:57 +0200

    per-route guard

--- src/router/index.js
+++ src/router/index.js
@@ -2,53 +2,49 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import HomeView from '../views/HomeView.vue'
 
+function guest() {
+  const { isAuthenticated } = useAuthStore()
+
+  if (isAuthenticated) {
+    return { name: 'vehicles' }
+  }
+}
+
+function auth() {
+  const { isAuthenticated } = useAuthStore()
+
+  if (!isAuthenticated) {
+    return { name: 'login' }
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-      meta: {
-        auth: false
-      }
+      component: HomeView
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/Auth/LoginView.vue'),
-      meta: {
-        auth: false
-      }
+      beforeEnter: guest
     },
     {
       path: '/register',
       name: 'register',
       component: () => import('@/views/Auth/RegisterView.vue'),
-      meta: {
-        auth: false
-      }
+      beforeEnter: guest
     },
     {
       path: '/vehicles',
       name: 'vehicles',
       component: () => import('@/views/Vehicles/ListView.vue'),
-      meta: {
-        auth: true
-      }
+      beforeEnter: auth
     }
   ]
-})
-
-// Route guard for auth routes
-router.beforeEach((to) => {
-  const { isAuthenticated } = useAuthStore()
-
-  if (to.matched.some((response) => response.meta.auth)) {
-    if (!isAuthenticated && to.name !== 'login') {
-      return { name: 'login' }
-    }
-  }
 })
 
 export default router
