54f505b9342794fa146e8bf5297406de096e643c
Tomáš Reinert <reinerttomas@gmail.com>
2023-07-22 18:42:46 +0200

    vehicles list

--- src/api/vehicle.js
+++ src/api/vehicle.js
@@ -1,5 +1,12 @@
 import { useAxios } from '@/lib/axios'
 
+export const getVehicles = async () => {
+  const { client } = useAxios()
+  const { data } = await client.get('vehicles')
+
+  return data
+}
+
 export const storeVehicle = async (vehicle) => {
   const { client } = useAxios()
   const { data } = await client.post('vehicles', vehicle)
--- src/views/Vehicles/ListView.vue
+++ src/views/Vehicles/ListView.vue
@@ -8,8 +8,47 @@
 
     <div class="border-t h-[1px] my-6"></div>
 
-    <div>There will be our vehicles list.</div>
+    <div class="flex flex-col gap-2">
+      <div
+        v-for="vehicle in vehicles"
+        :key="vehicle.id"
+        class="flex bg-gray-100 w-full p-2 justify-between"
+      >
+        <div class="flex items-center overflow-hidden w-full">
+          <div class="text-xl plate">
+            {{ vehicle.plateNumber }}
+          </div>
+          <div class="font-normal text-gray-600 pl-2 grow truncate">
+            {{ vehicle.description }}
+          </div>
+        </div>
+        <div class="flex gap-1">
+          <button type="button" class="btn btn-secondary text-sm">Edit</button>
+          <button type="button" class="btn text-white bg-red-600 hover:bg-red-500 text-sm">
+            X
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { getVehicles } from '@/api/vehicle'
+
+const vehicles = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await getVehicles()
+    vehicles.value = data
+  } catch (error) {
+    console.error(error)
+
+    /**
+     * TODO: add error message
+     */
+  }
+})
+</script>
